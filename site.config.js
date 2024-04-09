29b8a5ac8434f2b2fc39ac45a16f594849cd63e4
victortayo <152844504+victortayo@users.noreply.github.com>
2024-04-09 14:44:50 +0100

    Update site.config.js name

--- site.config.js
+++ site.config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Victor Tayo",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "curator",
     bio: "Can we make things better?",
